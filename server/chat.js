ceef86b884ed7c5583a5afc5865d4582bcb52ac0
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-09-22 01:07:24 +0800

    create server/chat.js

--- server/chat.js
+++ server/chat.js
@@ -10,12 +10,12 @@ import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 
 // NOTE: change this default filePath to any of your default file name
 const chat = async (filePath = "./uploads/hbs-lean-startup.pdf", query) => {
-// step 1:
+    // step 1:
     const loader = new PDFLoader(filePath);
 
     const data = await loader.load();
 
-// step 2:
+    // step 2:
     const textSplitter = new RecursiveCharacterTextSplitter({
         chunkSize: 500, //  (in terms of number of characters)
         chunkOverlap: 0,
@@ -23,7 +23,7 @@ const chat = async (filePath = "./uploads/hbs-lean-startup.pdf", query) => {
 
     const splitDocs = await textSplitter.splitDocuments(data);
 
-// step 3
+    // step 3
 
     const embeddings = new OpenAIEmbeddings({
         openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
@@ -34,29 +34,29 @@ const chat = async (filePath = "./uploads/hbs-lean-startup.pdf", query) => {
         embeddings
     );
 
-// step 4: retrieval
+    // step 4: retrieval
 
-// const relevantDocs = await vectorStore.similaritySearch(
-// "What is task decomposition?"
-// );
+    // const relevantDocs = await vectorStore.similaritySearch(
+    // "What is task decomposition?"
+    // );
 
-// step 5: qa w/ customzie the prompt
+    // step 5: qa w/ customzie the prompt
     const model = new ChatOpenAI({
         modelName: "gpt-3.5-turbo",
         openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
     });
 
     const template = `Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible.
-
-{context}
-Question: {question}
-Helpful Answer:`;
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    
+    {context}
+    Question: {question}
+    Helpful Answer:`;
 
     const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
         prompt: PromptTemplate.fromTemplate(template),
-// returnSourceDocuments: true,
+    // returnSourceDocuments: true,
     });
 
     const response = await chain.call({
